6f7069cef73a6258bdc1c0bfc3b0162eac45a900
Furkan Erdoğan <liuhawkeye@gmail.com>
2023-02-13 03:52:13 +0300

    etc.

---
+++ Examples/Notebook/Ex1.swift
@@ -0,0 +1,9 @@
+//A student's name, age, height, and the variable holding the first letter of their name
+import UIKit
+
+var ad = "Furkan"
+var yas = 24
+var boy = 1.83
+var harf = "F"
+
+print()
---
+++ Examples/Notebook/Page1.swift
@@ -0,0 +1,35 @@
+//
+//  page1.swift
+//  Page1
+//
+//  Created by Furkan Erdoğan on 13.02.2023.
+//
+
+import Foundation
+
+
+var d1 : Int
+d1 = 6
+print(d1)
+
+var d2 : Int = 67
+var d3 = 54
+
+var d4 = 5.6
+var d5 : Float = 5.7
+
+var d6 : String = "text"
+var d7 = "text2"
+
+var d8 = true
+var d9 : Bool = false
+
+//let d10 : Int = 10
+var d10 : Int = 10
+d10 = 234 //Cannot assign to value: 'd10' is a 'let' constant, using 'var' will fix this.
+
+let text = "Hello, \"Blue\" \n\tand \\Big\\ World"
+print(text)
+//
+
+
